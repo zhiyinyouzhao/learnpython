6c9a055a4c832e0d8e22c2bdebaaaf927dd0e38c
zhaoning <zhaoning@qq.com>
2021-09-26 17:35:14 +0800

    0926

---
+++ 装饰器/07装饰器-装饰器语法-类装饰器装饰函数.py
@@ -0,0 +1,26 @@
+
+
+class Outer():
+
+    #魔术方法：放把该类对象当作函数调用时，自动触发obj()
+    def __call__(self, func):
+        self.func = func   #把传进来的函数作为对象的成员方法
+        return self.inner   #返回一个函数
+
+
+    def inner(self,who):
+        print('拿到妹子的微信')
+        self.func(who)
+        print('看一场午夜电影')
+
+
+
+@Outer()            #outer() ==> obj @obj ==>obj(love) ==> __call__(love) ==> inner()
+def love(who):
+    print(f'{who}和妹子谈谈人生和理想...')
+
+
+
+
+if __name__ == '__main__':
+    love('张三')
