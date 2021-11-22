6b5c553c8c6e79a1440b678e75a7f796f514277a
zhaoning <5251870@qq.com>
2021-11-22 22:37:07 +0800

    11.22-1

---
+++ 函数/高阶函数-闭包函数.py
@@ -0,0 +1,45 @@
+
+
+money = 0
+
+# def work():
+#     global money
+#     money += 100
+
+'''
+闭包特点：
+    1.在外函数中定义了局部变量，并且在内部函数中使用了这个局部变量
+    2.在外函数中返回了内涵数，返回的内涵数就是闭包函数
+    3.主要在于保护了外函数中的局部变量，既可以被使用，又不会被破坏
+
+'''
+
+def person():
+    money = 0
+
+    def work():
+        nonlocal money      #在内涵数中使用了外函数的临时变量
+        money += 100
+        print(money)
+
+    # 在外函数中返回了内涵数，这个内涵数就是闭包函数
+    return work
+    # def overtime():
+    #     nonlocal money
+    #     money += 200
+    #
+    # def buy():
+    #     nonlocal money
+    #     money -= 50
+    #
+
+
+if __name__ == '__main__':
+    res = person()
+    res()
+    res()
+
+# 如何检测一个函数时否为闭包函数?
+# 函数名.__closure__,如果是闭包函数返回cell
+    print(person.__closure__)
+    print(res.__closure__)
